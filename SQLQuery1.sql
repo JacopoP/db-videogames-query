09d475c617710731ed31362a67e016d1bededfe4
Jac22 <jacopo.prudentino@gmail.com>
2023-04-19 15:00:25 +0200

    Completate query group by

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,6 @@
-﻿SELECT * FROM software_houses
+﻿-- Singola tabella
+SELECT * FROM software_houses
 WHERE country = 'United States' 
-
 
 SELECT * FROM players
 WHERE city = 'Rogahnland'
@@ -34,3 +34,27 @@ WHERE videogame_id = 412
 
 SELECT COUNT(*) FROM videogames
 WHERE software_house_id = 1 AND YEAR(release_date) = 2018
+
+
+
+
+
+-- query group by
+SELECT COUNT(*) FROM software_houses
+GROUP BY country
+
+SELECT videogame_id, COUNT(*) FROM reviews
+GROUP BY videogame_id
+
+SELECT pegi_label_id, COUNT(*) FROM pegi_label_videogame
+GROUP BY pegi_label_id
+
+SELECT YEAR (release_date), COUNT(*) FROM videogames
+GROUP BY YEAR(release_date)
+
+SELECT device_id, COUNT(*) FROM device_videogame
+GROUP BY device_id
+
+SELECT videogame_id, AVG(rating) AS reviews_avg FROM reviews
+GROUP BY videogame_id
+ORDER BY reviews_avg
